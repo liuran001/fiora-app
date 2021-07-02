ba0e08125275169a434be4f0711292e872b7fde1
liuran001 <32791471+liuran001@users.noreply.github.com>
2021-07-02 09:00:00 +0800

    Update Image.tsx

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Image as BaseImage, ImageSourcePropType } from 'react-native';
-import { getOSSFileUrl } from '../utils/uploadFile';
-import { referer } from '../utils/constant';
 
 type Props = {
     src: string;
@@ -14,21 +12,15 @@ export default function Image({ src, width = '100%', height = '100%', style }: P
     // @ts-ignore
     let source: ImageSourcePropType = src;
     if (typeof src === 'string') {
-        let uri = getOSSFileUrl(src, `image/quality,q_80`);
-        if (width !== '100%' && height !== '100%') {
-            uri = getOSSFileUrl(
-                src,
-                `image/resize,w_${Math.ceil(width as number)},h_${Math.ceil(
-                    height as number,
-                )}/quality,q_80`,
-            );
+        let prefix = '';
+        if (src.startsWith('//')) {
+            prefix = 'http:';
+        } else if (src.startsWith('/')) {
+            prefix = 'http://chat.qqcn.xyz:9200';
         }
         source = {
-            uri: uri as string,
+            uri: prefix + src,
             cache: 'force-cache',
-            headers: {
-                Referer: referer,
-            },
         };
     }
     return <BaseImage source={source} style={[style, { width, height }]} />;
