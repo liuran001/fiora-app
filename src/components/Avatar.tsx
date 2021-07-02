c733627425767d108e25fe266f5d55f59ba5f10a
liuran001 <32791471+liuran001@users.noreply.github.com>
2021-07-02 08:54:35 +0800

    Update Avatar.tsx

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { getOSSFileUrl } from '../utils/uploadFile';
 
 import Image from './Image';
 
@@ -8,6 +7,12 @@ type Props = {
     size: number;
 };
 export default function Avatar({ src, size }: Props) {
-    const targetUrl = getOSSFileUrl(src, `image/resize,w_${size * 2},h_${size * 2}/quality,q_90`) as string;
-    return <Image src={targetUrl} width={size} height={size} style={{ borderRadius: size / 2 }} />;
+    return (
+        <Image
+            src={src}
+            width={size}
+            height={size}
+            style={{ borderRadius: size / 2 }}
+        />
+    );
 }
